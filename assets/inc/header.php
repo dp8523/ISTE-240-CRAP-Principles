b7713356adde96a419a2487cbaf86e371dc9d40f
Xander-W-Wise <143832636+Xander-W-Wise@users.noreply.github.com>
2024-11-13 11:13:30 -0800

    Fixed header.php link

--- assets/inc/header.php
+++ assets/inc/header.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="../../assets/css/styles.css" rel="stylesheet">
+        <link href="assets/css/styles.css" rel="stylesheet">
         <style>
             @import url('https://fonts.googleapis.com/css2?family=Kameron:wght@400..700&display=swap');
             @import url('https://fonts.googleapis.com/css2?family=Goldman:wght@400;700&family=Kameron:wght@400..700&display=swap');
