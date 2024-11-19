7c21e531d86f8e182392e58f1a2509d83b042baf
Xander-W-Wise <143832636+Xander-W-Wise@users.noreply.github.com>
2024-11-19 14:19:45 -0800

    Added the base content and styled my pages

--- pages/repetition/connectedness.php
+++ pages/repetition/connectedness.php
@@ -4,7 +4,24 @@
     require_once('../../assets/inc/header.php');
 ?>
 
-<h1>Information here</h1>
+<div class="pageTitle">
+    <h1>Law of Uniform Connectedness</h1>
+</div>
+
+<div class="tightText">
+<p> For the concept of repetition the Law of Uniform Connectedness is very useful. 
+    It is a Gestalt idea that essentially said people connect things together based on similar designs and styles. 
+    For repetition this is a critical idea as repetition is all about setting 
+    a style to make your website more iconic or memorable to the user. 
+    Knowing this principle you can more clearly understand the importance of picking colors, shapes, and typography 
+    of similar style as those designs need to work together in order to make the user feel more connected and
+    comfortable on the page.
+</p>
+</div>
+
+<div class="imgCenter">
+<img src="../../assets/images/law.jpg" alt="Law of Connectedness Example">
+</div>
 
 <?php
     require_once("../../assets/inc/footer.php");
--- pages/repetition/movement.php
+++ pages/repetition/movement.php
@@ -4,7 +4,27 @@
     require_once('../../assets/inc/header.php');
 ?>
 
-<h1>Information here</h1>
+<div class="pageTitle">
+    <h1>Movement</h1>
+</div>
+
+<div class="tightText">
+<p>
+Movement on a webpage can add a lot of quality of life to the user experience. 
+In general the idea of using movement on a website is to grab user attention and to make the page feel more 
+interactive and alive. 
+There are many different ways in which we can get a website to move. 
+Some of the most common things websites use are slight size adjustments to objects being 
+hovered over, pieces of videos played in the background, and moving panels that slide into view when scrolling 
+down a page. 
+Adjusting the positioning of elements by making them shake or grow when hovered over is very helpful as the user 
+can more accurately tell what they were interacting with.
+</p>
+</div>
+
+<div class="imgCenter">
+    <img src="../../assets/images/movement1.gif" alt="Movement on webpage gif">
+<div>
 
 <?php
     require_once("../../assets/inc/footer.php");
