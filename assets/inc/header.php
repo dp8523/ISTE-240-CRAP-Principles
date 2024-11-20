134c99deb748fe7bad86282488d618ef164632c8
dp8523 <dp8523@g.rit.edu>
2024-11-20 16:05:20 -0500

    changed header.php to have .php in all links except homepage, contrast now includes header.php

--- assets/inc/header.php
+++ assets/inc/header.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="assets/css/styles.css" rel="stylesheet">
+        <link href="../../assets/css/styles.css" rel="stylesheet">
         <style>
             @import url('https://fonts.googleapis.com/css2?family=Kameron:wght@400..700&display=swap');
             @import url('https://fonts.googleapis.com/css2?family=Goldman:wght@400;700&family=Kameron:wght@400..700&display=swap');
@@ -14,33 +14,33 @@
         <div class="nav">
             <ul>
                 <li class="active"><a href="../../index.html">Home</a></li>
-                <li><a href="../contrast/contrast.html">Contrast</a>
+                <li><a href="../contrast/contrast.php">Contrast</a>
                     <div class="subnav">
                         <ul>
-                            <li><a href="../contrast/color.html">Color and Texture</a></li>
+                            <li><a href="../contrast/color.php">Color and Texture</a></li>
                         </ul>
                     </div>
                 </li>
-                <li><a href="../repetition/repetition.html">Repetition</a>
+                <li><a href="../repetition/repetition.php">Repetition</a>
                     <div class="subnav">
                         <ul>
                             <li><a href="../repetition/connectedness.php">Uniform Connectedness</a></li>
-                            <li><a href="../repetition/typography.html">Typography</a></li>
+                            <li><a href="../repetition/typography.php">Typography</a></li>
                             <li><a href="../repetition/movement.php">Movement</a></li>
                         </ul>
                     </div>
                 </li>
-                <li><a href="../alignment/alignment.html">Alignment</a>
+                <li><a href="../alignment/alignment.php">Alignment</a>
                     <div class="subnav">
                         <ul>
-                            <li><a href="../alignment/whitespace.html">White Space</a></li>
+                            <li><a href="../alignment/whitespace.php">White Space</a></li>
                         </ul>
                     </div>
                 </li>
-                <li><a href="../proximity/proximity.html">Proximity</a>
+                <li><a href="../proximity/proximity.php">Proximity</a>
                     <div class="subnav">
                         <ul>
-                            <li><a href="../proximity/card_design.html">Card Design</a></li>
+                            <li><a href="../proximity/card_design.php">Card Design</a></li>
                             <li><a href="../proximity/hicks_law.php">Hick's Law</a></li>
                         </ul>
                     </div>
