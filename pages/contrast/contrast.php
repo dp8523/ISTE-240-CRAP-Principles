d06a8aedb6d4e641a3497d039d635b3ab1d58e99
dp8523 <dp8523@g.rit.edu>
2024-11-20 14:30:14 -0500

    changed requires in contrast to header.inc.php and footer.inc.php

--- pages/contrast/contrast.php
+++ pages/contrast/contrast.php
@@ -1,11 +1,11 @@
 <?php
     $title = 'Contrast';
 
-    require_once('../../assets/inc/header.php');
+    require_once('../../assets/inc/header.inc.php');
 ?>
 
 <h1>Information here</h1>
 
 <?php
-    require_once("../../assets/inc/footer.php");
+    require_once("../../assets/inc/footer.inc.php");
 ?>
