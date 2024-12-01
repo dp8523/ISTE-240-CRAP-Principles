c73bef4d0abc8746a525a36a44a8250052598b8b
Sparks <sparks@wuffs.net>
2024-12-01 17:28:37 -0500

    prox text push

--- pages/proximity/proximity.php
+++ pages/proximity/proximity.php
@@ -9,6 +9,37 @@
 </div>
 
 
+<div class="tightText">
+<p>
+Proximity is used a lot on Canva's website. Proximity helps to create hierarchy and section off elements on the page. On Canva’s site there are multiple layouts with titles and descriptions and on the image you can see that they are close enough to the image of the layout that you can tell it's tied to it. Additionally the navigation on the left of the screen is positioned in a way that you can still see the majority of the page and access all the elements. They also put all the different formats underneath headings such as “Trending apps” or “New and noteworthy”. They place the titles closer to the formats to make it clear that they are part of that section.
+</p>
+</div>
+
+<!-- Image of Canva's website -->
+<div class="imgCenter">
+    <img src="../../assets/images/proximity1.jpg" alt="Canva's website">
+</div>
+
+<div class="tightText">
+<p>
+Proximity is in some ways a lot like alignment. They both help to establish hierarchy and decrease clutter. The major difference is that proximity is more specific to the elements that are already aligned. It is about placing elements into groupings and then spacing groupings to create a visually distinct hierarchy. For example if there is an image on a website, the text for that image would be right above or below it and keep close proximity, that way people can more easily view the content you are looking to convey. It can also help when grouping certain pieces of information together. If we were to group the major components of C.R.A.P. on the same page, you would want each letter in the acronym to be in its own dedicated section, not just thrown together in one massive paragraph as it would be easier to understand.
+</p>
+</div>
+
+<div class="imgCenter">
+    <img src="../../assets/images/proximity2.jpg" alt="Proximity example">
+</div>
+
+<br>
+
+<!-- Different example of proximity -->
+<div class="tightText">
+<p>
+In this section there are 2 images with text beneath them. The major distance is the space between them. If you were able to scroll on the page, when you scroll down you would not be able to tell if the text on the right is there for the image or if it is there for the text below. Proximity is very important because if you want similar or dependent pieces of information, you need them to be clustered together so that people can easily put together the information you are trying to communicate.
+</p>
+</div>
+
+<!-- CSS & html of prox-->
 
 <?php
     require_once("../../assets/inc/footer.php");
